d668d1864c3c5778e9295a8defc821f18daa1740
Tim Winters <61638296+erasersleeve@users.noreply.github.com>
2020-06-22 12:31:01 -0400

    Added more movie details to page for consideration, fixed missing .css on linkBtn

--- assets/consolidation.js
+++ assets/consolidation.js
@@ -19,7 +19,7 @@ $("#searchBtn").on("click", function(event){
     // Empties the divs, so that when the user inputs a new search, they can immediately identify their most recent search results.
     bookDiv.empty();
     movieDiv.empty();
-    // Send an AJAX call for the assembled URL to get the book title.
+    // Send an AJAX call for the assembled URL to get the book information.
     $.ajax({
     url:googleUrl,
     method:"GET"
@@ -37,7 +37,7 @@ $("#searchBtn").on("click", function(event){
         if (bookGet.items[i].saleInfo.buyLink) { 
         var buyLink = bookGet.items[i].saleInfo.buyLink; 
         var linkBtn = $("<button>");
-        linkBtn.text("Read on Google Books");linkBtn.attr("onclick", 'window.location.href= \'' + buyLink + '\';');linkBtn.attr("class", "btn blue lighten-1 waves-effect waves-light");
+        linkBtn.text("Read on Google Books");linkBtn.attr("onclick", 'window.location.href= \'' + buyLink + '\';');linkBtn.attr("class", "btn blue lighten-1 waves-effect waves-light");linkBtn.css({"margin": "10px"});
         } 
         else {var buyLink="No link available to purchase book."}
 
@@ -87,7 +87,7 @@ $("#searchBtn").on("click", function(event){
         method: "GET"
     }).then(function(movieGet) {
         // console.log(movieGet);
-      
+
       for (i=0; i<movieGet.Search.length; i++) {
     
         var searchIMDBid = movieGet.Search[i].imdbID;
@@ -95,17 +95,22 @@ $("#searchBtn").on("click", function(event){
         // URL Base for API.
         var idURL = "https://www.omdbapi.com/?i=" + searchIMDBid + "&apiKey=" + omdbKey;
         
-        // Send an AJAX call for the assembled URL to get various details of the movie from the array.
+        // Send an AJAX call for the assembled URL to get information 
         $.ajax({ 
           url:idURL,
           method: "GET"
         }).then (function(idGet) {
-          // console.log(idGet);
+          console.log(idGet);
           if (idGet.Title) {var movieTitle = idGet.Title}else{var movieTitle =" "}
-          if (idGet.Director) {var director = idGet.Director}else{var director ="No director information available."}
+          if (idGet.Director!=="N/A") {var director = idGet.Director}else{var director ="No director information available."}
           if (idGet.Plot) {var movieSynop = idGet.Plot}else{var movieSynop ="No synopsis available."}
           if (idGet.Poster && idGet.Poster !=="N/A") {var moviePoster = idGet.Poster}else{var moviePoster =""}
-          if (idGet.Ratings[0]) {var imdbRating = idGet.Ratings[0]}else{var imdbRating =" "}
+          if (idGet.Ratings[0]) {var imdbRating = "IMDB rating: "+idGet.Ratings[0].Value}else{var imdbRating =" "}
+          //I don't know that the blank variable strings will have the desired results in this case and the case above. Poster should be fine
+          //Rated will return N/A from api if there is nothing. Changing it so simply not exist
+          if (idGet.Rated!=="N/A") {var rated = idGet.Rated}else{var rated="MPA rating not available"}
+          if (idGet.Runtime) {var runtime = idGet.Runtime}else{var runtime=""}
+          if (idGet.Released){var released = idGet.Released}else{var released=""}
           
           // Variables to make new HTML elements for the card details (movie title, director, rating, and synopsis) and button is created to watch trailers.
           var movieResultsCard = $("<div>");
@@ -114,6 +119,9 @@ $("#searchBtn").on("click", function(event){
           var movieCardDirector = $("<p>");
           var movieCardSynop = $("<p>");
           var movieCardRating = $("<p>");
+          var movieCardRated = $("<p>");
+          var movieCardRuntime = $("<p>");
+          var movieCardReleased = $("<p>");
           var trailerBtn = $("<button>");
           var lineBreak = $("<br>");
           movieCoverImg.attr("src", moviePoster);
@@ -121,6 +129,10 @@ $("#searchBtn").on("click", function(event){
           movieCardDirector.html(director);
           movieCardSynop.html(movieSynop);
           movieCardRating.html(imdbRating);
+          movieCardRated.html(rated);
+          movieCardRuntime.html(runtime);
+          movieCardReleased.html(released);
+
           
           // Button displayed. The title of the movie that is pulled is put into a variable where it is split and then separated by dashes.
           trailerBtn.text("Find Trailer Here");
@@ -139,6 +151,9 @@ $("#searchBtn").on("click", function(event){
           movieResultsCard.append(lineBreak);
           movieResultsCard.append(movieCoverImg);
           movieResultsCard.append(movieCardRating);
+          movieResultsCard.append(movieCardRated);
+          movieResultsCard.append(movieCardRuntime);
+          movieResultsCard.append(movieCardReleased);
           movieResultsCard.attr("class", "card");
           movieDiv.append(movieResultsCard);
           movieResultsDiv.append(movieDiv);
